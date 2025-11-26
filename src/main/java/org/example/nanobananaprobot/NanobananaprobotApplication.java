216c62adad468b2d4625f2b4ecbad76d9d65e9b9
mariosb84 <mariosb84@mail.ru>
2025-11-26 16:43:08 +0300

    Build_26.11.2025

---
+++ src/main/java/org/example/nanobananaprobot/NanobananaprobotApplication.java
@@ -0,0 +1,13 @@
+package org.example.nanobananaprobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanobananaprobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanobananaprobotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/nanobananaprobot/NanobananaprobotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nanobananaprobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanobananaprobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
